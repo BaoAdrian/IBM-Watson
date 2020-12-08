66bcf379bd53ad70c58254a3f3ba7e0fa51848e3
Adrian Bao <adrianbao14@gmail.com>
2020-12-08 16:23:05 -0700

    Add constants used throughout program

---
+++ src/main/Constants.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Constants {
+    // Path to questions
+    public static final String pathToQuestions = "questions.txt";
+
+    // Index methods
+    public static final String NONE = "1";
+    public static final String LEMMA = "2";
+    public static final String STEMMING = "3";
+
+    // Pre-defined locations to store generated index
+    public static final String defaultIndexPath   = "index/default";
+    public static final String lemmaIndexPath     = "index/lemma";
+    public static final String stemmingIndexPath  = "index/stemming";
+}
