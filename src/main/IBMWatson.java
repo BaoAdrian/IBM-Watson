c3c8785aa38b39bff312a3ce3c9f718483b9577e
Adrian Bao <adrianbao14@gmail.com>
2020-12-08 16:24:05 -0700

    Add menus & update processing from Main

--- src/main/IBMWatson.java
+++ src/main/IBMWatson.java
@@ -1,23 +1,67 @@
 package main;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class IBMWatson {
-
-    public static String dataPath = "src/resources";
+    // Optional flags
+    public static boolean useLemma = false;
+    public static boolean useStemming = false;
+    public static boolean useBooleanQuery = false;
+    public static boolean useBM25Query = false;
 
     public static void main(String[] args) {
-        System.out.println("This is the start of the project");
+        System.out.println("Welcome to IBM Watson Lite!\n");
 
-        IndexEngine indexEngine = new IndexEngine("src/resources");
+        Scanner sc = new Scanner(System.in);
 
-        try {
-            indexEngine.parseWikiData();
-        } catch (IOException ex) {
-            System.out.println("EXCEPTION OCCURRED");
-            System.exit(0);
+        String goAgain = "";
+        String indexMethod = "";
+        String queryMethod = "";
+
+        // Continue processing requests until user quits
+        while(!goAgain.equals("n")) {
+            printBuildMenu();
+            indexMethod = sc.next().trim();
+
+            printQueryMenu();
+            queryMethod = sc.next().trim();
+
+            IndexEngine indexEngine = new IndexEngine(indexMethod);
+            QueryEngine queryEngine = new QueryEngine(indexEngine, queryMethod, indexMethod);
+            try {
+                System.out.println("Building index...");
+                indexEngine.parseWikiData();
+
+                System.out.println("Processing queries...");
+                queryEngine.processQuestions();
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                System.exit(0);
+            }
+
+            System.out.println("Would you like to go again? (y/n)");
+            goAgain = sc.next().trim();
         }
+
+        // User chose to exit, shutdown with message
+        System.out.println("\nTerminating program...");
     }
 
+    public static void printBuildMenu() {
+        System.out.println("How would you like to build the index?");
+        System.out.println(" (1) None");
+        System.out.println(" (2) Use Lemmatization");
+        System.out.println(" (3) Use Stemming");
+        System.out.print("> ");
+    }
 
+    public static void printQueryMenu() {
+        System.out.println("\nHow would you like perform queries?");
+        System.out.println(" (1) Boolean");
+        System.out.println(" (2) TF-IDF");
+        System.out.println(" (3) BM25");
+        System.out.print("> ");
+    }
 }
