39636a46cdaa77be67645a874eb6c33910ef4513
Adrian Bao <adrianbao14@gmail.com>
2020-12-09 13:37:50 -0700

    Update result class impl

--- src/main/Result.java
+++ src/main/Result.java
@@ -3,6 +3,29 @@ package main;
 import org.apache.lucene.document.Document;
 
 public class Result {
-    Document DocName;
-    double docScore = 0;
+    private Document DocName;
+    private double docScore = 0;
+
+    public Result(Document DocName, double docScore) {
+        this.DocName = DocName;
+        this.docScore = docScore;
+    }
+
+    /**
+     * Public accessor of the DocName attribute
+     *
+     * @return DocName instance variable
+     */
+    public Document getDocName() {
+        return this.DocName;
+    }
+
+    /**
+     * Public accessor of the docScore attribute
+     *
+     * @return docScore instance variable
+     */
+    public double getDocScore() {
+        return this.docScore;
+    }
 }
